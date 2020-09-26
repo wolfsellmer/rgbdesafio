cf1fb0dd4a06f7f11c5c61ba8a0f21e036cd3764
wolfsellmer <rp.wolfsellmer@gmail.com>
2020-09-26 20:22:26 -0300

    blueandredoK

--- scripts.js
+++ scripts.js
@@ -1,73 +1,73 @@
 //window.addEventListener('load', start)
 
 
-const redColor = null
-const greenColor = null
-const blueColor = null
-const red = null
-const green = null
-const blue = null
-const rgbResult = null
+// const redColor = null
+// const greenColor = null
+// const blueColor = null
+// const red = null
+// const green = null
+// const blue = null
+// const rgbResult = null
 
 
 
-function start(){
-    red = document.querySelector('#r');
-    green = document.querySelector('#g');
-    blue = document.querySelector('#b');
+// function start(){
+//     red = document.querySelector('#r');
+//     green = document.querySelector('#g');
+//     blue = document.querySelector('#b');
 
-    redColor = document.querySelector('#redValue');
-    greenColor = document.querySelector('#greenValue');
-    blueColor = document.querySelector('#blueValue');
-    
-    rgbResult = document.querySelector('#rgb');
+//     redColor = document.querySelector('#redValue');
+//     greenColor = document.querySelector('#greenValue');
+//     blueColor = document.querySelector('#blueValue');
 
-    redColor.addEventListener('input', rgbColor);
-    greenColor.addEventListener('input',rgbColor);
-    blueColor.addEventListener('input', rgbColor);
-    rgbColor()
+//     rgbResult = document.querySelector('#rgb');
+
+//     redColor.addEventListener('input', rgbColor);
+//     greenColor.addEventListener('input',rgbColor);
+//     blueColor.addEventListener('input', rgbColor);
+//     rgbColor()
+// }
+
+// function rgbColor(){
+//     const cRed = parseInt(redColor.value, 10);
+//     const cGreen = parseInt(greenColor.value, 10);
+//     const cBlue = parseInt(blueColor.value, 10);
+
+//     redColor.value = cRed;
+//     greenColor.value = cGreen;
+//     blueColor.value = cBlue;
+
+//     const rgbNew = 'rgb('+ cRed + ',' + cGreen + ',' + cBlue + ')';
+
+//     rgbResult.style.backgroundColor = rgbNew
+// }
+// start();
+
+
+
+const red = document.querySelector('#r')
+const green = document.querySelector('#g')
+const blue = document.querySelector('#b')
+const redColor = document.querySelector('#redValue')
+const greenColor = document.querySelector('#greenValue')
+const blueColor = document.querySelector('#blueValue')
+const rgbResult = document.querySelector('#rgb')
+
+
+red.oninput = ()=>{
+    redColor.value = parseInt(red.value)
+    rgbResult.value = 'rgb(' + parseInt(redColor.value)+ ',' +parseInt(greenColor.value)+ ',' + parseInt(blueColor.value) +')'
+    document.getElementById("rgb").style.backgroundColor = rgbResult.value
 }
-
-function rgbColor(){
-    const cRed = parseInt(redColor.value, 10);
-    const cGreen = parseInt(greenColor.value, 10);
-    const cBlue = parseInt(blueColor.value, 10);
-
-    redColor.value = cRed;
-    greenColor.value = cGreen;
-    blueColor.value = cBlue;
-
-    const rgbNew = 'rgb('+ cRed + ',' + cGreen + ',' + cBlue + ')';
-
-    rgbResult.style.backgroundColor = rgbNew
+green.oninput = ()=>{
+    greenColor.value = parseInt(green.value)
+    rgbResult.value = 'rgb(' + parseInt(redColor.value)+ ',' +parseInt(greenColor.value)+ ',' + parseInt(blueColor.value) +')'
+    document.getElementById("rgb").style.backgroundColor = rgbResult.value
 }
-start();
-
-
-
-// const red = document.querySelector('#r')
-// const green = document.querySelector('#g')
-// const blue = document.querySelector('#b')
-// const redColor = document.querySelector('#redValue')
-// const greenColor = document.querySelector('#greenValue')
-// const blueColor = document.querySelector('#blueValue')
-// const rgbResult = document.querySelector('#rgb')
-
-
-// red.oninput = ()=>{
-//     redColor.value = parseInt(red.value)
-//     rgbResult.value = 'rgb(' + parseInt(blueColor.value)+ ',' +parseInt(greenColor.value)+ ',' + parseInt(redColor.value)+')'
-//     document.getElementById("rgb").style.backgroundColor = rgbResult.value
-// }
-// green.oninput = ()=>{
-//     greenColor.value = parseInt(green.value)
-//     rgbResult.value = 'rgb(' + parseInt(blueColor.value)+ ',' +parseInt(greenColor.value)+ ',' + parseInt(redColor.value)+')'
-//     document.getElementById("rgb").style.backgroundColor = rgbResult.value
-// }
-// blue.oninput = ()=>{
-//     blueColor.value = parseInt(blue.value)
-//     rgbResult.value = 'rgb(' + parseInt(blueColor.value)+ ',' +parseInt(greenColor.value)+ ',' + parseInt(redColor.value)+')'
+blue.oninput = ()=>{
+    blueColor.value = parseInt(blue.value)
+    rgbResult.value = 'rgb(' + parseInt(redColor.value)+ ',' +parseInt(greenColor.value)+ ',' + parseInt(blueColor.value) +')'
     
-//     document.getElementById("rgb").style.backgroundColor = rgbResult.value
+    document.getElementById("rgb").style.backgroundColor = rgbResult.value
 
-// }
+}
